33359ca1e7a9abe0c89c41d064d7acd2cf659066
EtherealArd <vaultec114@gmail.com>
2018-05-08 23:07:49 +0300

    Xaml Home

---
+++ CropMonitoring/Helpers/PlotSeriesWorker.cs
@@ -0,0 +1,27 @@
+﻿using CropMonitoring.Model;
+using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CropMonitoring.Helpers
+{
+    class DataSeries
+    {
+        public double Rx { set; get; }
+        public double Ry { set; get; }
+    }
+
+    class PlotSeriesWorker
+    {
+        
+        public IEnumerable<DataSeries> GetSeries(List<VHIData> list,int year)
+        {
+            return list.Where(x => x.Year == year)
+                .Select(x => new DataSeries { Rx = x.Week, Ry = x.VHI });
+        }
+
+    }
+}
--- CropMonitoring/ViewModel/HomeViewModel.cs
+++ CropMonitoring/ViewModel/HomeViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using CropMonitoring.DataWorkers;
+using CropMonitoring.Helpers;
 using CropMonitoring.Infrastructure;
 using CropMonitoring.Model;
+using OxyPlot;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,7 +13,15 @@ using System.Windows.Input;
 
 namespace CropMonitoring.ViewModel
 {
-    class HomeViewModel:ViewModelBase
+    public static class Extensions
+    {
+        public static ObservableCollection<T> ToObservableCollection<T>(this IEnumerable<T> col)
+        {
+            return new ObservableCollection<T>(col);
+        }
+    }
+
+    class HomeViewModel : ViewModelBase
     {
         ObservableCollection<VHIData> _vhiData;
         public ObservableCollection<VHIData> VHIData
@@ -20,7 +30,7 @@ namespace CropMonitoring.ViewModel
             {
                 if (_vhiData == null)
                     return new ObservableCollection<Model.VHIData>();
-                   return _vhiData;                     
+                return _vhiData;
             }
             set
             {
@@ -29,7 +39,7 @@ namespace CropMonitoring.ViewModel
         }
 
 
-        
+
 
         RelayCommand _getVHIDataCommand;
         public ICommand GetVHIData
@@ -42,7 +52,7 @@ namespace CropMonitoring.ViewModel
             }
         }
 
-       
+
         public void ExecuteGetVHIDataCommand(object parameter)
         {
             VHIDataContext.ReadFromFile((string)parameter);
@@ -117,15 +127,15 @@ namespace CropMonitoring.ViewModel
             try
             {
                 int year = Convert.ToInt32(parameter);
-                if(year>=1981&&year<=2018)
-                Extremums = _dataWorker.GetExtremums(year, VHIData);
+                if (year >= 1981 && year <= 2018)
+                    Extremums = _dataWorker.GetExtremums(year, VHIData);
             }
-            catch(Exception)
+            catch (Exception)
             {
 
             }
-            
-            
+
+
             OnPropertyChanged("Extremums");
         }
         public bool CanExecuteGetExtremumCommand(object parameter)
@@ -134,7 +144,137 @@ namespace CropMonitoring.ViewModel
                 return false;
             return true;
         }
+        
+        DataPoint[] _dataSeries;
+        public DataPoint[] DataSeries
+        {
+            get
+            {
+                return _dataSeries;
+            }
+            set
+            {
+                _dataSeries = value;
+            }
+        }
 
+        RelayCommand _makePlot;
+        public ICommand MakePlot
+        {
+            get {
+                if (_makePlot == null)
+                    _makePlot = new RelayCommand(ExecuteGetPlotSeriesCommand, CanExecuteGetPlotSeriesCommand);
+                return _makePlot;
+            }
+        }
+
+        public void ExecuteGetPlotSeriesCommand(object parameter)
+        {
+            PlotSeriesWorker seriesWorker = new PlotSeriesWorker();
+
+            if (VHIData == null)
+                return;
+
+            try
+            {
+                int year = Convert.ToInt32(parameter);
+                List<VHIData> vhiList = VHIData.ToList<VHIData>();
+                DataSeries = seriesWorker.GetSeries(vhiList, year).Select(x => new DataPoint(x.Rx, x.Ry)).ToArray();
+            }
+
+            catch (Exception)
+            {
+
+            }
+            OnPropertyChanged("DataSeries");
+
+        }
+
+        public bool CanExecuteGetPlotSeriesCommand(object parameter)
+        {
+            if (string.IsNullOrEmpty((string)parameter))
+                return false;
+            return true;
+        }
+
+        DataPoint[] _dataSeries2;
+        public DataPoint[] DataSeries2
+        {
+            get
+            {
+                return _dataSeries2;
+            }
+            set
+            {
+                _dataSeries2 = value;
+            }
+        }
+
+        RelayCommand _makePlot2;
+        public ICommand MakePlot2
+        {
+            get
+            {
+                if (_makePlot2 == null)
+                    _makePlot2 = new RelayCommand(ExecuteGetPlotSeries2Command, CanExecuteGetPlotSeries2Command);
+                return _makePlot2;
+            }
+        }
+
+        public void ExecuteGetPlotSeries2Command(object parameter)
+        {
+            PlotSeriesWorker seriesWorker = new PlotSeriesWorker();
+
+            if (VHIData == null)
+                return;
+
+            try
+            {
+                int year = Convert.ToInt32(parameter);
+                List<VHIData> vhiList = VHIData.ToList<VHIData>();
+                DataSeries2 = seriesWorker.GetSeries(vhiList, year).Select(x => new DataPoint(x.Rx, x.Ry)).ToArray();
+            }
+
+            catch (Exception)
+            {
+
+            }
+            OnPropertyChanged("DataSeries2");
+
+        }
+
+        public bool CanExecuteGetPlotSeries2Command(object parameter)
+        {
+            if (string.IsNullOrEmpty((string)parameter))
+                return false;
+            return true;
+        }
+
+
+        RelayCommand _clearPlot;
+        public ICommand ClearPlot
+        {
+            get
+            {
+                if (_clearPlot == null)
+                    _clearPlot = new RelayCommand(ExecuteClearCommand, CanClearCommand);
+                return _clearPlot;
+            }
+        }
+
+        public void ExecuteClearCommand(object parameter)
+        {
+            DataSeries = null;
+            DataSeries2 = null;
+
+            OnPropertyChanged("DataSeries");
+            OnPropertyChanged("DataSeries2");
+        }
+
+        public bool CanClearCommand(object parameter)
+        {
+            return true;
+        }
 
     }
 }
